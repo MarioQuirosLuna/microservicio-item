0bca60e449eba9bf0014a7335bc5eaf3c62128c7
MarioQL <mquiros@epromcr.com>
2025-01-06 15:01:59 -0600

    Fix imports

--- src/main/java/com/example/springcloud/microservicio/items/services/ItemServiceFeign.java
+++ src/main/java/com/example/springcloud/microservicio/items/services/ItemServiceFeign.java
@@ -6,7 +6,7 @@ import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
+//import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.example.springcloud.microservicio.items.clients.ProductFeignClient;

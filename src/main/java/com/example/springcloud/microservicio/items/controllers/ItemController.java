15f483c7feb81c14e2f99087ebdf79bea4fcb45b
MarioQL <mquiros@epromcr.com>
2025-01-09 09:10:47 -0600

    Actuator para refrescar variables de configuracion

--- src/main/java/com/example/springcloud/microservicio/items/controllers/ItemController.java
+++ src/main/java/com/example/springcloud/microservicio/items/controllers/ItemController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@RefreshScope
 @RestController
 public class ItemController {
 
